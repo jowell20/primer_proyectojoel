21e51bbf9951b5c1f34185b5b7d4ea2df876fa46
jowell20 <joelssolanillag@gmail.com>
2020-03-14 00:08:01 -0500

    se agrega funcion addmascota y listarmascota

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,9 @@
 $(function(){
+  $('.contenedor_derecha2').hide();
+  $('#tablam').hide();
 agregarpropietario();
-
+addmascota();
+listarmascota();
 });
 
 
@@ -25,4 +28,39 @@ $("#bot").on('click', function(e){
    "<td><a> <button type='submit' class='btn btn-primary AgregarMascota' id='et' > Agregar Mascota</button></a></td>" +   "</tr>")
     }
 }  )
+}
+function addmascota(){
+
+$('#tablep').on('click' , '.AgregarMascota' , function(e)
+{
+e.preventDefault();
+alert('entro');
+$('.contenedor_derecha2').show();
+
+var tr = $(this).closest('tr');
+
+var tdpropietario = tr.children("td:nth-child(1)").html();
+  $('#labelPropietario').html(tdpropietario);
+
+
+});
+}
+ function listarmascota(){
+  $('#botMascota').on('click' ,function(e){
+    e.preventDefault();
+   $('#tablam').show();
+   e.preventDefault();
+     var nombrem = $("#mascota1");
+    var tipom = $("#mascota2");
+    var fecham = $("#mascota3");
+    if( nombrem.val()=="" || tipom.val()==""||fecham.val()==""){
+      alert("debe de llenar todos los espacios vacios");
+
+    }else{
+      $('#tablam').append("<tr>" +
+      "<td>" + nombrem.val() + "</td>" +
+      "<td>" + tipom.val() + "</td>" +
+      "<td>" + fecham.val() + "</td>")
+    }
+  });
 }
